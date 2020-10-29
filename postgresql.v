c4a450c612871a319e380fb5fc5e2873e9f7a690
Rolf Schmidt <rolf@schmidt.ink>
2020-10-29 20:56:51 +0100

    try postgres action

--- postgresql.v
+++ postgresql.v
@@ -1,18 +1,18 @@
 module holla
 
-//import pg
-//
-//pub fn postgresql_example() bool {
-//    config := pg.Config{
-//        host: 'localhost'
-//        user: 'postgres'
-//        password: 'postgres'
-//        dbname: 'holla'
-//    }
-//    db := pg.connect(config) or {
-//        panic('driver init failed:$err')
-//    }
-//    db.close()
-//
-//    return true
-//}
+import pg
+
+pub fn postgresql_example() bool {
+    config := pg.Config{
+        host: '127.0.0.1'
+        user: 'postgres'
+        password: 'postgres'
+        dbname: 'holla'
+    }
+    db := pg.connect(config) or {
+        panic('driver init failed:$err')
+    }
+    db.close()
+
+    return true
+}
--- postgresql_test.v
+++ postgresql_test.v
@@ -1,5 +1,5 @@
 module holla
 
-//fn test_example() {
-//    assert postgresql_example() == true
-//}
+fn test_example() {
+    assert postgresql_example() == true
+}
